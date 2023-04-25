fe2bc420a8c725334209c1eef4c3ccd196c2743a
lkeix <nakamura-keisuke2@kcgrp.jp>
2023-04-25 11:23:59 +0900

    add: test

--- greeting/greeting_test.go
+++ greeting/greeting_test.go
@@ -3,6 +3,8 @@ package greeting_test
 import (
 	"fmt"
 	"testing"
+
+	"github.com/lkeix/inspect-coverage-saas/greeting"
 )
 
 func TestNewHoge(t *testing.T) {
@@ -22,6 +24,27 @@ func TestHogeGreeting(t *testing.T) {
 	for _, testcase := range testcases {
 		t.Run(testcase.name, func(t *testing.T) {
 			fmt.Println("write your unit test!")
+		})
+	}
+}
+func TestNewFuga(t *testing.T) {
+	testcases := []struct {
+		name string
+	}{{name: ""}}
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+			fmt.Println("write your unit test!")
+		})
+	}
+}
+func TestFugaGreeting(t *testing.T) {
+	testcases := []struct {
+		name string
+	}{{name: "test"}}
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+			f := greeting.NewFuga("hoge")
+			f.Greeting()
 		})
 	}
 }
