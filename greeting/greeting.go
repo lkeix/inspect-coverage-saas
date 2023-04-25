a8addd9332ca5566e6ac58ed0b875acc83a7a2e7
lkeix <nakamura-keisuke2@kcgrp.jp>
2023-04-25 11:16:03 +0900

    fix

--- greeting/greeting.go
+++ greeting/greeting.go
@@ -1,15 +1,19 @@
 package greeting
 
+import (
+	"fmt"
+)
+
 type Hoge struct {
-  name string
+	name string
 }
 
 func NewHoge(name string) *Hoge {
-  return &Hoge{
-    name: name,
-  }
+	return &Hoge{
+		name: name,
+	}
 }
 
 func (h *Hoge) Greeting() {
-  fmt.Println("hello " + h.name + "!")
+	fmt.Println("hello " + h.name + "!")
 }
